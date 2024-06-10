e6f858277a0697d78ac239758a60733baf19dabf
Muhammad Ridho Amrullah <ridhoamrullah99@gmail.com>
2024-06-10 10:05:09 +0700

    Release 1 - Authentication Login: Done

--- cms-site/src/router.jsx
+++ cms-site/src/router.jsx
@@ -1,10 +1,33 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function checkLogin() {
+  if (!localStorage.access_token) {
+    return redirect("/login");
+  }
+  return null;
+}
 
 const router = createBrowserRouter([
   {
     path: "/register",
     element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    loader: () => {
+      if (localStorage.access_token) {
+        return redirect("/");
+      }
+      return null;
+    },
+  },
+  {
+    path: "/",
+    element: <p>Hai</p>,
+    loader: checkLogin,
   },
 ]);
 
